34edab3e6e10e0fc53654b74bd73578e3e122273
Tsion <tsionwendmu12@gmail.com>
2024-03-19 20:16:10 -0400

    created the layout for the app

---
+++ src/_auth/forms/SignupForms.tsx
@@ -0,0 +1,37 @@
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
+
+
+const SignupForms = () => {
+ 
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+   
+  )
+}
+
+export default SignupForms
